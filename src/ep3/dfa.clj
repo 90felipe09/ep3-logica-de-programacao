e53123300c9ee25fbc4c5896a2a4f8302537360e
90felipe09 <felhipeshiraishi@gmail.com>
2021-03-02 18:48:37 -0300

    add: dfa step function

---
+++ src/ep3/dfa.clj
@@ -0,0 +1,18 @@
+(ns ep3.dfa (:gen-class))
+(require '[ep3.computationStructure :as cs])
+
+(defn step
+  "returns a new computing state by applying a given transition function over
+  an input computing state and a tape value."
+  [computing-state]
+  {
+    :E ( 
+        ( 
+            (computing-state :T) (computing-state :E) 
+        )
+            (cs/readTape computing-state)
+        )
+    :T (computing-state :T)
+    :F (cs/reconfigTape computing-state)
+    }   
+)
