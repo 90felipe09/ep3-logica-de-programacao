dce9cf770dbd102ad638c38abeff27fbe4677cc6
Matheus Andrade <matheus.oa@usp.br>
2021-03-06 22:18:30 -0300

    [WIP] Conserta as funções de nfa exceto o loop de simulação

--- src/ep3/nfa.clj
+++ src/ep3/nfa.clj
@@ -16,49 +16,73 @@
   [transitions input]
   (select-keys transitions [input "eps"]))
 
-(defn step
-  "Returns the set of possible next computations based on an
-  initial set of computations with a non-deterministic transition function"
+(defn get-next-computations
+  "Returns the set of next computations based on an
+  initial computation with a non-deterministic transition function"
   [computing-state]
   (let [ { states :E
            transitions :T
            tape :F } computing-state ]
-  (println (transitions (first states)))
-  (for [state states
-        valid-transition (filter-valid-transitions (transitions state) (first tape))]
-    { :E (flatten (rest valid-transition))
-      :T transitions
-      :F (if (is-epsilon-transition? valid-transition) tape (drop 1 tape)) })))
+    (for [state states
+          valid-transition (filter-valid-transitions (transitions state) (subs tape 0 1))]
+      { :E (second valid-transition)
+        :T transitions
+        :F (if (is-epsilon-transition? valid-transition) tape (subs tape 1)) })))
 
-(defn is-final-computation?
-  [computation]
-  (< (count (computation :T)) 1))
+(defn step
+  "Returns the set of possible next computations based on an
+  initial set of computations with a non-deterministic transition function"
+  [computing-state-list]
+  (first (concat (map #(get-next-computations %) computing-state-list))))
 
-(defn is-valid-final-computation?
-  [ computation final-states ]
-  (let [ { state :E
-           transitions :T
-           tape :F } computation ]
-    (and (is-final-computation? computation) (contains? final-states state))))
-
-(defn exists-valid-final-computation?
-  [reached-computation-list final-states]
-  (reduce 
-    (fn [acc computation]
-      (if (is-valid-final-computation? computation)
-        (reduced true)
-      ;else
-        acc))
-    false ;first acc
-    reached-computation-list))
+(defn is-tape-empty?
+  "Returns whether the received tape is empty or not"
+  [tape]
+  (= (count tape) 0))
 
 (defn exists-only-final-computations?
-  [reached-computation-list final-states]
+  "Returns whether the supplied computation list only has final computations (empty tape)"
+  [reached-computation-list]
   (reduce
     (fn [acc computation]
-      (if (is-final-computation? computation)
+      (if (is-tape-empty? (computation :F))
         acc
       ;else
         (reduced false)))
-  true
-  reached-computation-list))
+    true ;initial acc
+    reached-computation-list))
+
+(defn is-valid-final-computation?
+  "Returns whether a given computation's state list has a valid final state"
+  [computation final-states]
+  (let [ { states :E
+           transitions :T
+           tape :F } computation ]
+    (and (is-tape-empty? tape) (not= nil (some (set final-states) states)))))
+
+(defn exists-valid-final-computation?
+  "Returns whether the supplied computation list has at least one valid final computation"
+  [reached-computation-list final-states]
+  (reduce 
+    (fn [acc computation]
+      (if (is-valid-final-computation? computation final-states)
+        (reduced true)
+      ;else
+        acc))
+    false ;initial acc
+    reached-computation-list))
+
+(defn run-nfa
+  ""
+  [machine tape]
+  (let [initial-computation {:E [(machine :q)] :T (machine :T) :F tape}
+        final-states (machine :F)]
+    (loop [computation-list initial-computation]
+      (let [next-computation-list (step computation-list)]
+        (if (exists-valid-final-computation? next-computation-list final-states)
+          true
+        ;else
+          (if (exists-only-final-computations? next-computation-list)
+            false
+          ;else
+            (recur next-computation-list)))))))
--- test/ep3/core_test.clj
+++ test/ep3/core_test.clj
@@ -31,8 +31,64 @@
 
 (deftest step-test
   (testing "non-deterministic computation step"
-  (let [machine (fsmParser/parse-automata "resources/test-machine.nfa")
-        tape (slurp "resources/nfa-test-tape.tap")
-        initial-computation {:E '((machine :q)) :T (machine :T) :F tape}
-        resulting-computation (nfa/step initial-computation) ]
-    (println resulting-computation))))
+  (let [machine (fsmParser/parse-automata "resources/machines/test-machine.nfa")
+        tape (slurp "resources/tapes/nfa-test-tape.tap")
+        initial-computation {:E [(machine :q)] :T (machine :T) :F tape}
+        resulting-computations (nfa/get-next-computations initial-computation)]
+    (is (= '(["A"]) (map #(:E %) resulting-computations))))))
+
+(deftest two-step-test
+  (testing "nfa step chaining"
+  (let [machine (fsmParser/parse-automata "resources/machines/test-machine.nfa")
+        tape (slurp "resources/tapes/nfa-test-tape.tap")
+        initial-computation {:E [(machine :q)] :T (machine :T) :F tape}
+        first-step-computations (nfa/get-next-computations initial-computation)
+        second-step-computations (concat (map #(nfa/get-next-computations %) first-step-computations))])))
+
+(deftest is-tape-empty-test
+  (testing "check computation for empty tape"
+  (let [non-empty-tape "001"
+        empty-tape ""]
+    (is (= true (nfa/is-tape-empty? empty-tape)))
+    (is (= false (nfa/is-tape-empty? non-empty-tape))))))
+
+(deftest exists-only-final-computation-test
+  (testing "check computation list for finality"
+  (let [non-final-computation-list [{:E nil :T nil :F ""}
+                                    {:E nil :T nil :F "0"}]
+        final-computation-list [{:E nil :T nil :F ""}
+                                {:E nil :T nil :F ""}] ]
+    (is (= true (nfa/exists-only-final-computations? final-computation-list)))
+    (is (= false (nfa/exists-only-final-computations? non-final-computation-list))))))
+
+(deftest is-valid-final-computation-test
+  (testing "check computation for validity and finality"
+  (let [final-states ["B"]
+        non-final-non-valid-comp {:E ["A"] :T nil :F "0"}
+        non-final-valid-comp {:E ["B"] :T nil :F "0"}
+        final-non-valid-comp {:E ["A"] :T nil :F ""}
+        final-valid-comp {:E ["B"] :T nil :F ""}]
+    (is (= false (nfa/is-valid-final-computation? non-final-non-valid-comp final-states)))
+    (is (= true (nfa/is-valid-final-computation? final-valid-comp final-states)))
+    (is (= false (nfa/is-valid-final-computation? final-non-valid-comp final-states)))
+    (is (= false (nfa/is-valid-final-computation? non-final-valid-comp final-states))))))
+
+(deftest exists-valid-final-computation-test
+  (testing "check computation list for a state with finality and validity"
+  (let [final-states ["B"]
+        non-final-computation-list [{:E ["A"] :T nil :F ""}
+                                    {:E ["B"] :T nil :F "0"}
+                                    {:E ["A"] :T nil :F "0"}]
+        final-computation-list [{:E ["A"] :T nil :F ""}
+                                {:E ["B"] :T nil :F "0"}
+                                {:E ["B"] :T nil :F ""}
+                                {:E ["A"] :T nil :F "0"}] ]
+    (is (= true (nfa/exists-valid-final-computation? final-computation-list final-states)))
+    (is (= false (nfa/exists-valid-final-computation? non-final-computation-list final-states))))))
+
+(deftest run-nfa-test
+  (testing "check successful simulation"
+  (let [machine (fsmParser/parse-automata "resources/machines/test-machine.nfa")
+        tape (slurp "resources/tapes/nfa-test-tape.tap")]
+    (is (= true (nfa/run-nfa machine tape))))))
+
